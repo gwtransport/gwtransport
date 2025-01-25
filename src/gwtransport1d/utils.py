ecf516cc32d3119967a10bcd2cc9475f8488e79e
Bas des Tombe <bdestombe@gmail.com>
2025-01-25 09:18:34 +0100

    Add diff function to compute cell widths with time alignment options and corresponding tests

--- src/gwtransport1d/utils.py
+++ src/gwtransport1d/utils.py
@@ -86,3 +86,32 @@ def interp_series(series, index_new, **interp1d_kwargs):
     dt_interp = (index_new - series.index[0]) / pd.to_timedelta(1, unit="D")
     interp_obj = interpolate.interp1d(dt, series.values, bounds_error=False, **interp1d_kwargs)
     return interp_obj(dt_interp)
+
+
+def diff(a, time_alignment="centered"):
+    """Compute the cell widths for a given array of cell coordinates.
+
+    If time_alignment is "centered", the coordinates are assumed to be centered in the cells.
+    If time_alignment is "left", the coordinates are assumed to be at the left edge of the cells.
+    If time_alignment is "right", the coordinates are assumed to be at the right edge of the cells.
+
+    Parameters
+    ----------
+    a : array-like
+        Input array.
+
+    Returns
+    -------
+    array
+        Array with differences between elements.
+    """
+    if time_alignment == "centered":
+        mid = a[:-1] + (a[1:] - a[:-1]) / 2
+        return np.concatenate((a[[1]] - a[[0]], mid[1:] - mid[:-1], a[[-1]] - a[[-2]]))
+    if time_alignment == "left":
+        return np.concatenate((a[1:] - a[:-1], a[[-1]] - a[[-2]]))
+    if time_alignment == "right":
+        return np.concatenate((a[[1]] - a[[0]], a[1:] - a[:-1]))
+
+    msg = f"Invalid time_alignment: {time_alignment}"
+    raise ValueError(msg)
