12fff425e3948f9ee050a678529fd9c345605f31
Bas des Tombe <bdestombe@gmail.com>
2025-06-03 12:54:21 +0200

    Refactor residence_time function and enhance tests for flow timing parameters

--- src/gwtransport/residence_time.py
+++ src/gwtransport/residence_time.py
@@ -18,7 +18,16 @@ from gwtransport.utils import linear_average, linear_interpolate
 
 
 def residence_time(
-    flow=None, flow_tedges=None, flow_tstart=None, flow_tend=None, aquifer_pore_volume=None, index=None, retardation_factor=1.0, direction="extraction", return_pandas_series=False
+    flow=None,
+    flow_tedges=None,
+    flow_tstart=None,
+    flow_tend=None,
+    aquifer_pore_volume=None,
+    index=None,
+    retardation_factor=1.0,
+    direction="extraction",
+    *,
+    return_pandas_series=False,
 ):
     """
     Compute the residence time of retarded compound in the water in the aquifer.
@@ -26,14 +35,15 @@ def residence_time(
     Parameters
     ----------
     flow : pandas.Series, array-like
-        Flow rate of water in the aquifer [m3/day]. If flow_tedges is not defined, the index of `flow` is used.
+        Flow rate of water in the aquifer [m3/day]. The timestamps of the flow data should be aligned with the time edges provided in `flow_tedges`.
+        If left to None, the function will raise an error. The length of `flow` should match the length of `flow_tedges` minus one.
     flow_tedges : pandas.DatetimeIndex, optional
         Time edges for the flow data. If provided, it is used to compute the cumulative flow.
         If left to None, the index of `flow` is used. Has a length of one more than `flow`. Default is None.
-    flow_tstart : pandas.Timestamp, optional
+    flow_tstart : pandas.DatetimeIndex, optional
         Timestamps aligned to the start of the flow measurement intervals. Preferably use flow_tedges,
         but if not available this approach can be used for convenience. Has the same length as `flow`.
-    flow_tend : pandas.Timestamp, optional
+    flow_tend : pandas.DatetimeIndex, optional
         Timestamps aligned to the end of the flow measurement intervals. Preferably use flow_tedges,
         but if not available this approach can be used for convenience. Has the same length as `flow`.
     aquifer_pore_volume : float or array-like of float
@@ -45,6 +55,8 @@ def residence_time(
         Retardation factor of the compound in the aquifer [dimensionless].
     direction : str, optional
         Direction of the flow. Either 'extraction' or 'infiltration'. Extraction refers to backward modeling: how many days ago did this extracted water infiltrate. Infiltration refers to forward modeling: how many days will it take for this infiltrated water to be extracted. Default is 'extraction'.
+    return_pandas_series : bool, optional
+        If True, return a pandas Series with the residence time at the index provided. Only supported for a single aquifer pore volume.
 
     Returns
     -------
@@ -53,33 +65,7 @@ def residence_time(
     """
     aquifer_pore_volume = np.atleast_1d(aquifer_pore_volume)
 
-    if flow_tedges is not None:
-        flow_tedges = pd.DatetimeIndex(flow_tedges)
-        if len(flow) != len(flow_tedges) - 1:
-            msg = "flow_tedges must have one more element than flow"
-            raise ValueError(msg)
-
-    elif flow_tstart is not None:
-        # Assume the index refers to the time at the start of the measurement interval
-        flow_tstart = pd.DatetimeIndex(flow_tstart)
-        if len(flow) != len(flow_tstart):
-            msg = "flow_tstart must have the same number of elements as flow"
-            raise ValueError(msg)
-
-        flow_tedges = pd.concat([flow_tstart, [flow_tstart[-1] + (flow_tstart[-1] - flow_tstart[-2])]])
-
-    elif flow_tend is not None:
-        # Assume the index refers to the time at the end of the measurement interval
-        flow_tend = pd.DatetimeIndex(flow_tend)
-        if len(flow) != len(flow_tend):
-            msg = "flow_tend must have the same number of elements as flow"
-            raise ValueError(msg)
-
-        flow_tedges = pd.concat([[flow_tend[0] - (flow_tend[1] - flow_tend[0])], flow_tend])
-
-    else:
-        msg = "Either provide flow_tedges, flow_tstart, and flow_tend"
-        raise ValueError(msg)
+    flow_tedges = _compute_time_edges(flow_tedges, flow_tstart, flow_tend, len(flow))
 
     flow_tedges_days = np.asarray((flow_tedges - flow_tedges[0]) / np.timedelta64(1, "D"))
     flow_tdelta = np.diff(flow_tedges_days, prepend=0.0)
@@ -89,7 +75,7 @@ def residence_time(
     if index is None:
         # If index is not provided return the residence time that matches with the index of the flow; at the center of the flow bin.
         index_dates_days_extraction = (flow_tedges_days[:-1] + flow_tedges_days[1:]) / 2
-        flow_cum_at_index = flow_cum
+        flow_cum_at_index = (flow_cum[:-1] + flow_cum[1:]) / 2  # at the center of the flow bin
     else:
         index_dates_days_extraction = np.asarray((index - flow_tedges[0]) / np.timedelta64(1, "D"))
         flow_cum_at_index = linear_interpolate(
@@ -116,6 +102,37 @@ def residence_time(
             raise ValueError(msg)
         return pd.Series(data=data[0], index=index, name=f"residence_time_{direction}")
     return data
+
+
+def _compute_time_edges(flow_tedges, flow_tstart, flow_tend, number_of_bins):
+    if flow_tedges is not None:
+        flow_tedges = pd.DatetimeIndex(flow_tedges)
+        if number_of_bins != len(flow_tedges) - 1:
+            msg = "flow_tedges must have one more element than flow"
+            raise ValueError(msg)
+
+    elif flow_tstart is not None:
+        # Assume the index refers to the time at the start of the measurement interval
+        flow_tstart = pd.DatetimeIndex(flow_tstart)
+        if number_of_bins != len(flow_tstart):
+            msg = "flow_tstart must have the same number of elements as flow"
+            raise ValueError(msg)
+
+        flow_tedges = flow_tstart.append(flow_tstart[[-1]] + (flow_tstart[-1] - flow_tstart[-2]))
+
+    elif flow_tend is not None:
+        # Assume the index refers to the time at the end of the measurement interval
+        flow_tend = pd.DatetimeIndex(flow_tend)
+        if number_of_bins != len(flow_tend):
+            msg = "flow_tend must have the same number of elements as flow"
+            raise ValueError(msg)
+
+        flow_tedges = (flow_tend[[0]] - (flow_tend[1] - flow_tend[0])).append(flow_tend)
+
+    else:
+        msg = "Either provide flow_tedges, flow_tstart, and flow_tend"
+        raise ValueError(msg)
+    return flow_tedges
 
 
 def residence_time_mean(
